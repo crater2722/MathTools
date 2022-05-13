a2cc44e9d9a211bfffbcc91b5d270be76c659aaa
therealtutugoy <103195306+therealtutugoy@users.noreply.github.com>
2022-05-13 10:11:28 -0400

    Caluculator

---
+++ Programs/Caluculator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int  main() {
+    float num1;
+    float num2;
+    char op;
+    float result;
+    printf("Enter the first number: \n");
+    scanf("%f",&num1);
+    printf("Enter the operator: \n");
+    scanf(" %c",&op);
+    printf("Enter the second number: \n");
+    scanf("%f",&num2);
+    switch (op)
+    {
+        case '-' :
+        result=num1-num2;
+        printf("%f",result);
+        break;
+        
+        case '+' :
+        result=num1+num2;
+        printf("%f",result);
+        break;
+        
+        case '*' :
+        result=num1*num2;
+        printf("%f",result);
+        break;
+        
+        case '/' :
+        result=num1/num2;
+        printf("%f",result);
+        break;
+        
+        default :
+        printf("Operator is not valid");
+    }
+    
+}
