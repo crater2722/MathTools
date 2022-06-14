51b5cd9131363b77935f59e30a7a2491d2684f39
therealtutugoy <103195306+therealtutugoy@users.noreply.github.com>
2022-06-14 18:43:47 +0530

    areaOfCircle program

---
+++ Programs/areaOfCircle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+float r,area;
+printf ("Enter radius:- \n");
+scanf("%f",&r);
+printf ("Entered radius is %f \n",r);
+area=(22/7)*r*r;
+printf ("\nThe Area of Circle is = %f",area);   
+    return 0;
+}
